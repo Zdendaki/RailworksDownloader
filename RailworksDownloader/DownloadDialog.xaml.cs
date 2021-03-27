ae64ad4476eecb72069bef93e6a19bc4e3100e72
JachyHm <jachym.hurtik@gmail.com>
2021-03-28 00:51:46 +0100

    download cancelation bugfix

--- RailworksDownloader/DownloadDialog.xaml.cs
+++ RailworksDownloader/DownloadDialog.xaml.cs
@@ -43,88 +43,86 @@ namespace RailworksDownloader
                     FileName.Content = p?.DisplayName ?? Localization.Strings.InvalidFileName;
                 });
 
-                await Task.Run(async () =>
-                {
-                    int pkgId = pair.Key;
-                    wrapper.OnDownloadProgressChanged += Wrapper_OnDownloadProgressChanged;
-                    try {
-                        ObjectResult<object> dl_result = await wrapper.DownloadPackage(pkgId, App.Token);
+                int pkgId = pair.Key;
+                wrapper.OnDownloadProgressChanged += Wrapper_OnDownloadProgressChanged;
 
-                        if (WebWrapper.CancelDownload)
-                            return;
+                try {
+                    ObjectResult<object> dl_result = await wrapper.DownloadPackage(pkgId, App.Token);
 
-                        if (Utils.IsSuccessStatusCode(dl_result.code))
+                    if (WebWrapper.CancelDownload)
+                        return;
+
+                    if (Utils.IsSuccessStatusCode(dl_result.code))
+                    {
+                        Dispatcher.Invoke(() => CancelButton = false);
+
+                        //cleanup before installig new version
+                        List<string> removedFiles = Utils.RemoveFiles(sqLiteAdapter.LoadPackageFiles(pkgId));
+                        sqLiteAdapter.RemovePackageFiles(removedFiles);
+
+                        List<string> installedFiles = new List<string>();
+                        List<string> failedFiles = new List<string>();
+                        using (ZipArchive a = ZipFile.OpenRead((string)dl_result.content))
                         {
-                            Dispatcher.Invoke(() => CancelButton = false);
-
-                            //cleanup before installig new version
-                            List<string> removedFiles = Utils.RemoveFiles(sqLiteAdapter.LoadPackageFiles(pkgId));
-                            sqLiteAdapter.RemovePackageFiles(removedFiles);
-
-                            List<string> installedFiles = new List<string>();
-                            List<string> failedFiles = new List<string>();
-                            using (ZipArchive a = ZipFile.OpenRead((string)dl_result.content))
+                            foreach (ZipArchiveEntry e in a.Entries)
                             {
-                                foreach (ZipArchiveEntry e in a.Entries)
+                                if (e.Name == string.Empty) //is directory
+                                    continue;
+
+                                string rel_assets_path = Utils.NormalizePath(Path.Combine(installedPackages.Where(x => x.PackageId == pkgId).Select(x => x.TargetPath).First(), e.FullName));
+                                string path = Path.GetDirectoryName(Path.Combine(App.Railworks.AssetsPath, rel_assets_path));
+
+                                try
                                 {
-                                    if (e.Name == string.Empty) //is directory
-                                        continue;
+                                    if (!Directory.Exists(path))
+                                        Directory.CreateDirectory(path);
 
-                                    string rel_assets_path = Utils.NormalizePath(Path.Combine(installedPackages.Where(x => x.PackageId == pkgId).Select(x => x.TargetPath).First(), e.FullName));
-                                    string path = Path.GetDirectoryName(Path.Combine(App.Railworks.AssetsPath, rel_assets_path));
-
-                                    try
-                                    {
-                                        if (!Directory.Exists(path))
-                                            Directory.CreateDirectory(path);
-
-                                        e.ExtractToFile(Path.Combine(path, e.Name), true);
-                                        installedFiles.Add(rel_assets_path);
-                                    }
-                                    catch
-                                    {
-                                        failedFiles.Add(e.FullName);
-                                    }
+                                    e.ExtractToFile(Path.Combine(path, e.Name), true);
+                                    installedFiles.Add(rel_assets_path);
+                                }
+                                catch
+                                {
+                                    failedFiles.Add(e.FullName);
                                 }
                             }
-
-                            Trace.Assert(failedFiles.Count == 0, Localization.Strings.FailedCopyFiles, string.Join("\n", failedFiles));
-
-                            sqLiteAdapter.SavePackageFiles(pkgId, installedFiles);
-                            installedPackages[installedPackages.FindIndex(x => x.PackageId == pkgId)] = p;
-                            sqLiteAdapter.SavePackage(p);
-                            new Task(() =>
-                            {
-                                sqLiteAdapter.FlushToFile(true);
-                            }).Start();
-
-                            Dispatcher.Invoke(() => CancelButton = true);
                         }
-                        else
+
+                        Trace.Assert(failedFiles.Count == 0, Localization.Strings.FailedCopyFiles, string.Join("\n", failedFiles));
+
+                        sqLiteAdapter.SavePackageFiles(pkgId, installedFiles);
+                        installedPackages[installedPackages.FindIndex(x => x.PackageId == pkgId)] = p;
+                        sqLiteAdapter.SavePackage(p);
+                        new Task(() =>
                         {
-                            new Task(() =>
-                            {
+                            sqLiteAdapter.FlushToFile(true);
+                        }).Start();
 
-                                App.Window.Dispatcher.Invoke(() =>
-                                {
-                                    MainWindow.ErrorDialog = new ContentDialog()
-                                    {
-                                        Title = Localization.Strings.DownloadError,
-                                        Content = dl_result.message,
-                                        SecondaryButtonText = Localization.Strings.Ok,
-                                        Owner = App.Window
-                                    };
-
-                                    MainWindow.ErrorDialog.ShowAsync();
-                                });
-
-                            }).Start();
-                        }
-
-                        File.Delete((string)dl_result.content);
+                        Dispatcher.Invoke(() => CancelButton = true);
                     }
-                    catch { }
-                });
+                    else
+                    {
+                        new Task(() =>
+                        {
+
+                            App.Window.Dispatcher.Invoke(() =>
+                            {
+                                MainWindow.ErrorDialog = new ContentDialog()
+                                {
+                                    Title = Localization.Strings.DownloadError,
+                                    Content = dl_result.message,
+                                    SecondaryButtonText = Localization.Strings.Ok,
+                                    Owner = App.Window
+                                };
+
+                                MainWindow.ErrorDialog.ShowAsync();
+                            });
+
+                        }).Start();
+                    }
+
+                    File.Delete((string)dl_result.content);
+                }
+                catch {break;}
             }
 
             App.Window.Dispatcher.Invoke(() => Hide());
@@ -240,7 +238,7 @@ namespace RailworksDownloader
 
                     File.Delete((string)dl_result.content);
                 } 
-                catch { }
+                catch {break;}
             }
 
             App.Window.Dispatcher.Invoke(() => Hide());
